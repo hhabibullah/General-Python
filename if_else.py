ea1d58bbe8dda98a22b3161d725eb47dd81d9a6e
Hamid <h.habibullah@student.fontys.nl>
2018-09-26 18:04:30 +0200

    if_Else statements

---
+++ if_else.py
@@ -0,0 +1,14 @@
+
+x = int(raw_input('Enter the number'))
+
+r = x%2
+if r==0:
+    print('Even')
+    if x>5:
+        print('you are great')
+    else:
+        print('You are not great')
+else:
+    print('odd')
+
+print('Have a nice time')
