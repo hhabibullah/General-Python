14f3dee5558cd7dcb6f44e50cfe8e6e7d84a68e5
Hamid <h.habibullah@student.fontys.nl>
2018-09-26 21:29:48 +0200

    while_loop

--- While_loop.py
+++ While_loop.py
@@ -1,9 +1,15 @@
 
 x = 1
+
 while x<5:
     print('Hello')
     x += 1
-x = 5
+#x = 5
 while x>=0:
     print('reverse order')
+    j =0
+    while j<=5:
+        print('break in loop')
+        j += 1
+
     x -= 1
