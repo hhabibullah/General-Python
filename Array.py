220399bbd04755110d1361463899b9740da70910
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 06:24:51 +0200

    array with (a for a in vals)

--- Array.py
+++ Array.py
@@ -6,7 +6,7 @@ from array import *
 
 vals = array('i',[1,2,3,4,5,6,78])
 
-newArr = array(vals.typecode,(a for a in vals))
+newArr = array(vals.typecode,(a*a for a in vals))
 #vals.reverse()
 #print(vals.buffer_info())
 for i in range(len(newArr)):
