35f429e01947a47090e513d945a651251529e634
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 09:45:19 +0200

    Array with Numpy package

--- Array.py
+++ Array.py
@@ -1,23 +1,8 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from array import *
+from numpy import *
 
-arr = array('i',[])
-n = int(raw_input('Enter the length of the array'))
-
-for i in range(n):
-    x = int(raw_input('Enter the next value'))
-    arr.append(x)
+arr = array([1,2,3,4])
 
 print(arr)
-k =0
-val = int(raw_input('Enter the value you are looking for'))
-for e in arr:
-    if e == val:
-        print(k)
-    k +=1
-# search the value by using fuction
-print('***************************************************')
-print('Search the value by using the function')
-print(arr.index(val))
