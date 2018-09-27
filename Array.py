5a5757ab86021013056c4e02da398163cfe61453
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 14:24:31 +0200

    Function with passing arguments

--- Array.py
+++ Array.py
@@ -1,19 +1,20 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 # one way of creating an array in Numpy package
-
+import numpy
 print('****************Working on Functions********************************')
 
-def greet():
-    print('Hello')
-    print('Good morning')
+def update(lst):
+    print(id(lst))
+    lst[0]=45
+    print('x',id(lst))
 
+a =10
+print(id(a))
+update(a)
+print('a',a)
+lst = [1,2,3,4,5,6,7,8]
+print(id(lst))
+update(lst)
+print('lst',lst)
 
-def sub_add(x,y):
-    c = x + y
-    d = x - y
-    return c,d
-
-greet()
-result,result1 = sub_add(5,7)
-print(result,result1)
