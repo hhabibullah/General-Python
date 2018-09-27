66f9d8c221b5721595926b5d65867bcb103e6c9d
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 03:53:40 +0200

    for loop with pattern triangle

--- For_loop.py
+++ For_loop.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 print('***********************************')
 for i in range(4):
-    for j in range(4):
+    for j in range(i+1):
         print('#',end ='')
     print()
 
