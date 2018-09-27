acc1126d5f5fe24ba5a99707c41ecc89e37aadc1
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 06:21:11 +0200

    Copying array using for loop

--- Array.py
+++ Array.py
@@ -4,14 +4,16 @@ from __future__ import unicode_literals
 from array import *
 
 
-vals = array('u',['a','f','e'])
+vals = array('i',[1,2,3,4,5,6,78])
+
+newArr = array(vals.typecode,(a for a in vals))
 #vals.reverse()
 #print(vals.buffer_info())
-for i in range(len(vals)):
+for i in range(len(newArr)):
 
     print(vals[i],end =' ')
 print()
 print('***********************************')
 
-for e in vals:
+for e in newArr:
     print(e , end = ' ')
