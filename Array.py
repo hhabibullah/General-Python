b2f5d5cfc591329faaf13f0c3d13412f2bf2e1ad
Hamid <h.habibullah@student.fontys.nl>
2018-09-28 01:14:39 +0200

    global vs local variables in functions

--- Array.py
+++ Array.py
@@ -1,32 +1,18 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
-# Here we talk about the position of the arguments that how much they are important if the position ar wrong then it will creat an error.
+a=10
+print('the outer variable address is ',id(a))
+def something():
+  #  global a
+    a=9
+    print('the local variable is ',a)
+    x = globals()['a']
+    print('the local variable address is ',id(x))
+    globals()['a'] = 15
 
-def person(name,age=18):
-
-    print(name)
-    print(age)
-    age = age-5
-    print(age)
-# Here we talk about the keyword argument that if the position is not correct still it will work by using the keyword
-
-
-person('Hamid',20) #position argument
-person(age = 20 ,name ='Hamid') #keyword argument
-
-# default argument if one of them is not assigned a value
-
-person('Hamid') #default argument
-
-def sum(name,**b): # Keyword Variable length argument
-    #print(name)
-    for i,j in b.items():
-        print(i,j)
-
-
-
-sum(name ='Hamid',city='Kabul',age = 20,mob =1234)
+something()
+print('the outer variable looks like',a)
 
 
 
