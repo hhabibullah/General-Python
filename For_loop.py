707891cdb2381b66d5ddb325ae7e5e88254e7e92
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 03:59:44 +0200

    For loop with reverse triangle

--- For_loop.py
+++ For_loop.py
@@ -2,11 +2,9 @@
 from __future__ import print_function
 print('***********************************')
 for i in range(4):
-    for j in range(i+1):
+    for j in range(4-i):
         print('#',end ='')
     print()
 
 
 print('***********************************')
-
-print()
