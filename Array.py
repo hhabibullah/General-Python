57f366302eded16e6839ec968127cc0abf730bcf
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 13:05:45 +0200

    Matrix Addition and Array Multiplication(using arrays)

--- Array.py
+++ Array.py
@@ -1,54 +1,38 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 # one way of creating an array in Numpy package
+
 from numpy import *
 
-arr1 = array([1,2,3,4,5])
-print('************Adding 5 to each element of array***************')
-arr1 = arr1 + 5
-print(arr1)
-arr2 = array([1,2,3,4,5])
-print('***********************Adding of two arrays ****************************')
-arr3 =  arr1 + arr2
+arr1 = array([[1,2,3,4,7,8],
+         [4,5,6,7,90,80]
+        ])
+arr5 = array([[8,9,0,1,2,3],[11,12,13,14,15,16]])
+print(arr1.ndim)
+print(arr1.dtype)
+print(arr1.shape)
+print(arr1.size)
+print('****************************************************************')
+arr2 = arr1.flatten()
+print(arr2)
+print('*****************************************************************')
+arr3 = arr2.reshape(3,4)
 print(arr3)
-print('***********************Finding SINE of each element**********************')
-print(sin(arr3))
-print('***********************Finding COS of each element************************')
-print(cos(arr3))
-print('***********************Finding LOG of each element*************************')
-print(log(arr3))
-print('************************Finding Square root of element************************')
-print(sqrt(arr3))
-print('************************Finding the sum of the array **************************')
-print(sum(arr3))
-print('***********************Finding the min of the arr3 ********************************')
-print(min(arr3))
-print('***********************Finding the max of the arr3*********************************')
-print(max(arr3))
-print('*********************** Concatenation of two arrays *******************************************')
-print(concatenate([arr1,arr2]))
-
-print('************************Copying one array to another ***************************************')
-
-arr5 = arr1
-print(id(arr1))
-print(id(arr5))
+m= matrix(arr1)
+print(m)
 
 
-print(arr1)
-print(arr5)
+print(m)
+print(diagonal(arr1))
+print(arr1.min())
+print(arr1.max())
 
+print('addition of two arrays ')
 
-print('***************************How to create two different array*****************************************************************')
+arr6 = arr1 + arr5
+print(arr6)
 
-arr5 = arr1.view()
-arr1[0]= 1000
-print(arr1)
-print(arr5)
-print('----------------------------------------------------------------')
-arr5 = arr1.copy()
-arr1[0]= 3000
-print(arr1)
-print(arr5)
+print('Multiplication of two arrays')
 
-print('***********************************************************************')
+arr7 = arr1 * arr5
+print(arr7)
