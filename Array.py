7dfda148536f8d6a3ecf0e22f91600bcb46a9c80
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 10:33:10 +0200

    Creating different types of array using Numpy package

--- Array.py
+++ Array.py
@@ -1,8 +1,36 @@
 from __future__ import print_function
 from __future__ import unicode_literals
-
+# one way of creating an array in Numpy package
 from numpy import *
-
-arr = array([1,2,3,4],int)
-
+arr = array([1,2,3.0,4],int)
+print(arr.dtype)
 print(arr)
+
+print('***********************************************************')
+print('another way of creating an array from Numpy Package')
+
+ar = linspace(0,15,20)
+
+print(ar)
+
+print('***********************************************************')
+print('another way of creating an array from Numpy Package')
+a = arange(1,15,2)
+print(a)
+print('***********************************************************')
+print('another way of creating an array from Numpy Package')
+
+aa = logspace(1,40,5)
+print('%.2f'%aa[1])
+
+print('***********************************************************')
+print('Another way of creating an array from Numpy Package')
+
+ap = zeros(5,int)
+print(ap)
+
+print('*********************************************************')
+print('Creating another type of array using Numpy package')
+
+s = ones(5,int)
+print(s)
