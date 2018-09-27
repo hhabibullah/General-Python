8ec0d5d64a6a6e6e05bc2e6ab0afb239c1ae3456
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 05:29:00 +0200

    Array with for loop

--- Array.py
+++ Array.py
@@ -1,5 +1,16 @@
+from __future__ import print_function
+
 from array import *
 
-vals =array('i',[1,2,3,4,5])
 
-print(vals)
+vals =array('i',[1,2,3,4,5])
+vals.reverse()
+print(vals.buffer_info())
+for i in range(len(vals)):
+
+    print(vals[i],end =' ')
+print()
+print('***********************************')
+
+for e in vals:
+    print(e , end = ' ')
