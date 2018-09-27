91d5662ae32063131b8ccb6c12edad63c23d7ab9
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 11:50:12 +0200

    Arrays (Addition of two arrays,adding element to the array,and different function with array)

--- Array.py
+++ Array.py
@@ -2,35 +2,53 @@ from __future__ import print_function
 from __future__ import unicode_literals
 # one way of creating an array in Numpy package
 from numpy import *
-arr = array([1,2,3.0,4],int)
-print(arr.dtype)
-print(arr)
 
-print('***********************************************************')
-print('another way of creating an array from Numpy Package')
+arr1 = array([1,2,3,4,5])
+print('************Adding 5 to each element of array***************')
+arr1 = arr1 + 5
+print(arr1)
+arr2 = array([1,2,3,4,5])
+print('***********************Adding of two arrays ****************************')
+arr3 =  arr1 + arr2
+print(arr3)
+print('***********************Finding SINE of each element**********************')
+print(sin(arr3))
+print('***********************Finding COS of each element************************')
+print(cos(arr3))
+print('***********************Finding LOG of each element*************************')
+print(log(arr3))
+print('************************Finding Square root of element************************')
+print(sqrt(arr3))
+print('************************Finding the sum of the array **************************')
+print(sum(arr3))
+print('***********************Finding the min of the arr3 ********************************')
+print(min(arr3))
+print('***********************Finding the max of the arr3*********************************')
+print(max(arr3))
+print('*********************** Concatenation of two arrays *******************************************')
+print(concatenate([arr1,arr2]))
 
-arr = linspace(0,15,20)
+print('************************Copying one array to another ***************************************')
 
-print(arr)
+arr5 = arr1
+print(id(arr1))
+print(id(arr5))
 
-print('***********************************************************')
-print('another way of creating an array from Numpy Package')
-arr = arange(1,15,2)
-print(arr)
-print('***********************************************************')
-print('another way of creating an array from Numpy Package')
 
-arr = logspace(1,40,5)
-print('%.2f'%arr[1])
+print(arr1)
+print(arr5)
 
-print('***********************************************************')
-print('Another way of creating an array from Numpy Package')
 
-arr = zeros(5,int)
-print(arr)
+print('***************************How to create two different array*****************************************************************')
 
-print('*********************************************************')
-print('Creating another type of array using Numpy package')
+arr5 = arr1.view()
+arr1[0]= 1000
+print(arr1)
+print(arr5)
+print('----------------------------------------------------------------')
+arr5 = arr1.copy()
+arr1[0]= 3000
+print(arr1)
+print(arr5)
 
-s = ones(5,int)
-print(s)
+print('*****************************************************************')
