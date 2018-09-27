44b5bc933aabf31503c3a2298797d0e7a72b0595
Hamid <h.habibullah@student.fontys.nl>
2018-09-27 15:38:52 +0200

    Types of arguments(default,position,variable length arguments,keyword argument)

--- Array.py
+++ Array.py
@@ -1,20 +1,30 @@
 from __future__ import print_function
 from __future__ import unicode_literals
-# one way of creating an array in Numpy package
-import numpy
-print('****************Working on Functions********************************')
 
-def update(lst):
-    print(id(lst))
-    lst[0]=45
-    print('x',id(lst))
+# Here we talk about the position of the arguments that how much they are important if the position ar wrong then it will creat an error.
 
-a =10
-print(id(a))
-update(a)
-print('a',a)
-lst = [1,2,3,4,5,6,7,8]
-print(id(lst))
-update(lst)
-print('lst',lst)
+def person(name,age=18):
+
+    print(name)
+    print(age)
+    age = age-5
+    print(age)
+# Here we talk about the keyword argument that if the position is not correct still it will work by using the keyword
+
+
+person('Hamid',20) #position argument
+person(age = 20 ,name ='Hamid') #keyword argument
+
+# default argument if one of them is not assigned a value
+
+person('Hamid') #default argument
+
+def sum(a,*b): # Variable length argument
+    c =a
+    for i in b:
+        c = c+i
+    print('the sum of tuple is ',c)
+
+sum(1,2,3,4)
+
 
