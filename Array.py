be7d89ce611f7996bdd939442d9066c25f839bba
Hamid <h.habibullah@student.fontys.nl>
2018-09-28 05:07:00 +0200

    Filter,Map and reduce with lambda expression

--- Array.py
+++ Array.py
@@ -1,15 +1,38 @@
 from __future__ import print_function
 from __future__ import unicode_literals
+from functools import reduce
+
+def is_even(n):
+    return n%2==0
+nums = [1,2,3,4,5,6,7,8,9]
+evens = list(filter(is_even,nums))
+print(evens)
+
+print('***************Can be done with lambda expression*********************************')
+
+evens = list(filter(lambda n : n%2 == 0,nums))
+
+print(evens)
+
+print('***************Using map function ************************************************')
 
 
+def update(n):
+    return n*2
 
-
-f = lambda a :a *a
-
-result = f(8)
+result = list(map(update,evens))
 print(result)
-print('**************************************************************')
-f = lambda b,c : b+c
-
-result = f(4,3)
+print('this can be done with lambda expression as well')
+result = list(map(lambda n : n*2,evens))
 print(result)
+
+print('***********************Using reduce function **************************************')
+
+def red(a,b):
+    return a +b
+res = reduce(red,result)
+print(res)
+print('This can be done by the help of lambda expression')
+
+res = reduce(lambda a,b:a+b,result)
+print(res)
