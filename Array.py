a1d5c98233a0634c87b13ec0ccc972a861feba1a
Hamid <h.habibullah@student.fontys.nl>
2018-09-28 08:49:15 +0200

    Modules in Function

--- Array.py
+++ Array.py
@@ -2,37 +2,19 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from functools import reduce
 
-def is_even(n):
-    return n%2==0
-nums = [1,2,3,4,5,6,7,8,9]
-evens = list(filter(is_even,nums))
-print(evens)
+import Calc
 
-print('***************Can be done with lambda expression*********************************')
-
-evens = list(filter(lambda n : n%2 == 0,nums))
-
-print(evens)
-
-print('***************Using map function ************************************************')
-
-
-def update(n):
-    return n*2
-
-result = list(map(update,evens))
+result = Calc.add(5,4)
 print(result)
-print('this can be done with lambda expression as well')
-result = list(map(lambda n : n*2,evens))
+print('********************')
+result = Calc.sub(9,5)
 print(result)
+print('********************')
 
-print('***********************Using reduce function **************************************')
+result = Calc.mult(5,4)
+print(result)
+print('******************')
 
-def red(a,b):
-    return a +b
-res = reduce(red,result)
-print(res)
-print('This can be done by the help of lambda expression')
-
-res = reduce(lambda a,b:a+b,result)
-print(res)
+result = Calc.div(20,4)
+print(result)
+print('******************')
