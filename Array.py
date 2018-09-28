52f2244dda7ad20fe199be8378c4e2e8773a0ce0
Hamid <h.habibullah@student.fontys.nl>
2018-09-28 16:14:26 +0200

    Types of method(static,class,instance)

--- Array.py
+++ Array.py
@@ -3,17 +3,35 @@ from __future__ import unicode_literals
 from functools import reduce
 
 
-class Car:
-    wheel = 4
-    def __init__(self):
-       self.mil = 10
-       self.model = 'BMW'
+class student:
 
+    school ='Applied'
+    def __init__(self,m1,m2,m3):
+        self.m1=m1
+        self.m2=m2
+        self.m3=m3
+    def avg(self):
+        return (self.m1+self.m2+self.m3)/3
+    def get_m(self):
+        return self.m1
+    def set_m(self,value):
+        self. m1=value
+        return self.m1
+    @classmethod
+    def getschool(cls):
+        return cls.school
+    @staticmethod
+    def info():
+        print('You are best dear')
 
-c1 = Car()
-c2 = Car()
-c1.mil =8
-Car.wheel = 3
+s1 = student(1,2,3)
+s2 = student(3,4,5)
+print('****************Schoolname************************')
+print(student.getschool())
+print('***************Static method*********************')
+print(student.info())
 
-print(c1.mil,c1.model,c1.wheel)
-print(c2.mil,c2.model,c2.wheel)
+print('getting the value of m',s1.get_m())
+print('setting the value of m',s1.set_m(60))
+print(s1.avg())
+print(s2.avg())
