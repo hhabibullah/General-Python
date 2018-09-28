2b9cd1ba742f06cb6ace56b62a7bf011df9aeda2
Hamid <h.habibullah@student.fontys.nl>
2018-09-28 02:04:57 +0200

    Finding factorial of a number by using function

--- Array.py
+++ Array.py
@@ -2,18 +2,12 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 
-def count(lst):
-  even =0
-  odd  =0
-  for i in lst:
-    if i%2==0:
-      even += 1
-    else:
-      odd += 1
-  return even,odd
+def fact(x):
+    f =1
+    for i in range(1,x+1):
+        f *=i
+    return f
 
-lst = [1,2,3,4,5,6,7,8]
-even,odd =count(lst)
-print('Even is {} and odd is {}'.format(even,odd))
-
-
+x = 5
+result= fact(x)
+print(result)
