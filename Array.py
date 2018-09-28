69cbe09b8d020e34bebcca8f43cd56f8759b898c
Hamid <h.habibullah@student.fontys.nl>
2018-09-28 17:48:47 +0200

    class with inner class

--- Array.py
+++ Array.py
@@ -5,33 +5,23 @@ from functools import reduce
 
 class student:
 
-    school ='Applied'
-    def __init__(self,m1,m2,m3):
-        self.m1=m1
-        self.m2=m2
-        self.m3=m3
-    def avg(self):
-        return (self.m1+self.m2+self.m3)/3
-    def get_m(self):
-        return self.m1
-    def set_m(self,value):
-        self. m1=value
-        return self.m1
-    @classmethod
-    def getschool(cls):
-        return cls.school
-    @staticmethod
-    def info():
-        print('You are best dear')
+    def __init__(self,name,rollno):
+        self.name = name
+        self.rollno= rollno
+        self.lap   = self.laptop()
 
-s1 = student(1,2,3)
-s2 = student(3,4,5)
-print('****************Schoolname************************')
-print(student.getschool())
-print('***************Static method*********************')
-print(student.info())
+    def show(self):
+        print(self.name,self.rollno)
+        self.lap.show()
+    class laptop:
+        def __init__(self):
+            self.brand ='HP'
+            self.ram   = 90
+        def show(self):
+            print(self.brand,self.ram)
 
-print('getting the value of m',s1.get_m())
-print('setting the value of m',s1.set_m(60))
-print(s1.avg())
-print(s2.avg())
+s1 =student('hamid', 1)
+s2 =student('Hafeez',2)
+
+print(s1.show())
+#lap1 = student.laptop()
